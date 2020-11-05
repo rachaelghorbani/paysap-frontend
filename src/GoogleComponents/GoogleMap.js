e67178081fe3a9fc65b6cbc6c178ba3fdaf1202c
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-11-05 09:56:20 -0500

    map stuff

--- src/GoogleComponents/GoogleMap.js
+++ src/GoogleComponents/GoogleMap.js
@@ -5,17 +5,19 @@ class GoogleMap extends React.Component {
 	state = {
 		showingInfoWindow: false,
 		activeMarker: {},
-        selectedPlace: {},
-        lat: null,
-        long: null
+		selectedPlace: {},
+		lat: null,
+		long: null,
+		// initLat: null,
+		// initLong: null
 	};
 	//want to bring in the find location on component did mount and reset initial center to those dots
 	// mapStyles = {
 	//     width: "88%",
 	//     height: "50%",
-    // }
-    
-    //may need to listen for user
+	// }
+
+	
 
 	containerStyle = {
 		marginTop: '10px',
@@ -25,19 +27,17 @@ class GoogleMap extends React.Component {
 	};
 
 	onMarkerClick = (props, marker, e) => {
-        
-        console.log(props)
+		console.log(props);
 		this.setState({
 			selectedPlace: props,
 			activeMarker: marker,
-            showingInfoWindow: true,
-            lat: props.position.lat,
-            long: props.position.lng
-            
+			showingInfoWindow: true,
+			lat: props.position.lat,
+			long: props.position.lng
 		});
 	};
 
-	onClose = (props) => {
+	onClose = () => {
 		if (this.state.showingInfoWindow) {
 			this.setState({
 				showingInfoWindow: false,
@@ -47,57 +47,79 @@ class GoogleMap extends React.Component {
 	};
 
 	renderMarker = () => {
-        
 		return this.props.jobs.map((job) => {
-            const refactoredDate = job.start_time.slice(0, 21)
-            const refactoredAddress = job.location.slice(0, job.location.length - 5)
-            return(
-			<Marker onClick={this.onMarkerClick} description={job.description} date={refactoredDate}location={refactoredAddress} position={{ lat: job.lat, lng: job.long }} />
-            )
-    });
-	};
- 
+			const refactoredDate = job.start_time.slice(0, 21);
+			const refactoredAddress = job.location.slice(0, job.location.length - 5);
+			return (
+				<Marker
+					key={job.id}
+					onClick={this.onMarkerClick}
+					description={job.description}
+					date={refactoredDate}
+					location={refactoredAddress}
+					position={{ lat: job.lat, lng: job.long }}
+				/>
+			);
+		});
+    };
+    
+    // would use these in production but takes longer to load so will use set lat and long for demo purposes
+
+	// getCoordinates = (position) => {
+	// 	const lat = parseFloat(JSON.stringify(position.coords.latitude), 10);
+	// 	const long = parseFloat(JSON.stringify(position.coords.longitude), 10);
+	// 	this.setState({ initLat: lat, initLong: long });
+	// };
+	// getLocation = () => {
+	// 	if (navigator.geolocation) {
+	// 		navigator.geolocation.getCurrentPosition(this.getCoordinates);
+	// 	} else {
+	// 		alert('geolocation not avail');
+	// 	}
+	// };
+	// componentDidMount = () => {
+	// 	this.getLocation();
+	// };
 
 	render() {
-        
-        console.log(this.props.google)
-		return (
-            <div style={{display: "flex", justifyContent: "center"}}> 
-			<Map
-				containerStyle={this.containerStyle}
-				google={this.props.google}
-				zoom={14}
-				// style={this.mapStyles}
-				initialCenter={{
-					lat: 42.4538356,
-					lng: -71.2337416
-                }}
-                //maybe set these in state and then onclick reset them for the individual marker
-				// center={{
-				// 	lat: this.state.lat,
-				// 	lng: this.state.long
-				// }}
-			>
-				{this.renderMarker()}
-				<InfoWindow
-					marker={this.state.activeMarker}
-					visible={this.state.showingInfoWindow}
-					onClose={this.onClose}
-				>
-					<div style={{textAlign: "center"}}>
-						<h6 style={{ width: 200 }}>{this.state.selectedPlace.description}
-                        </h6>
-                        <h6>
-                        {this.state.selectedPlace.date}
-                        </h6>
-                        <h6>
-                        {this.state.selectedPlace.location}
-                        </h6>
-					</div>
-				</InfoWindow>
-			</Map>
-            </div>
-		);
+		// if (this.state.initLong !== null && this.state.initLat !== null) {
+			return (
+				<div style={{ display: 'flex', justifyContent: 'center' }}>
+					<Map
+						containerStyle={this.containerStyle}
+						google={this.props.google}
+						zoom={14}
+						// style={this.mapStyles}
+						initialCenter={{
+							// lat: this.state.initLat,
+							// lng: this.state.initLong
+							lat: 42.4538356,
+							lng: -71.2337416
+						}}
+						//maybe set these in state and then onclick reset them for the individual marker
+						// center={{
+						// 	lat: this.state.lat,
+						// 	lng: this.state.long
+						// }}
+					>
+						{this.renderMarker()}
+						<InfoWindow
+							marker={this.state.activeMarker}
+							visible={this.state.showingInfoWindow}
+							onClose={this.onClose}
+						>
+							<div style={{ textAlign: 'center', width: 300 }}>
+								<h6>{this.state.selectedPlace.description}</h6>
+								<h6>{this.state.selectedPlace.date}</h6>
+								<h6>{this.state.selectedPlace.location}</h6>
+							</div>
+						</InfoWindow>
+					</Map>
+				</div>
+			);
+		// } else {
+		// 	return <div></div>;
+		// }
 	}
 }
 
