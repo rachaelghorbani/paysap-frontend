df75f08d728b8725cf26dcfbc7efc0c844f12d67
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-11-06 13:13:34 -0500

    changed freelanceJobsContainer into funtional component and used useEffect to reset date on unmounting

--- src/Containers/FreelanceJobsContainer.js
+++ src/Containers/FreelanceJobsContainer.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { connect } from 'react-redux';
 import { Table, Container} from 'react-bootstrap';
-// import CompletedFreelanceCard from '../Components/CompletedFreelanceJobCard';
 import OpenFreelanceCard from '../Components/OpenFreelanceCard'
 import GoogleMap from '../GoogleComponents/GoogleMap'
 import CompletedJobCard from '../Components/CompletedJobCard'
@@ -9,18 +8,15 @@ import DateFilterAndExcelRow, { filterByDate} from '../Components/DateFilterAndE
 import {setEndDateForFilter, setStartDateForFilter} from '../Redux/actions/SortActions'
 
 
-
-
-
-class FreelanceJobsContainer extends React.Component {
+const FreelanceJobsContainer = ({user, filterStartDate, filterEndDate, setStartDateForFilter, setEndDateForFilter}) => {
+    
   
-	usersCompletedFreelanceJobs = () => {
-        console.log(this.props.filterStartDate)
-		return filterByDate(this.props.user.jobs_as_freelancer, this.props.filterStartDate, this.props.filterEndDate, 'start_time', 'completed').map((job) => <CompletedJobCard key={job.id} job={job} email={job.client_email}/>);
+	const usersCompletedFreelanceJobs = () => {
+		return filterByDate(user.jobs_as_freelancer, filterStartDate, filterEndDate, 'start_time', 'completed').map((job) => <CompletedJobCard key={job.id} job={job} email={job.client_email}/>);
 	};
 
-	usersOpenFreelanceJobs = () => {
-        const openJobs = this.props.user.jobs_as_freelancer.filter((job) => job.completed === false);
+	const usersOpenFreelanceJobs = () => {
+        const openJobs = user.jobs_as_freelancer.filter((job) => job.completed === false);
         const sorted = () => {
             return openJobs.sort((a, b) => {
                 return Date.parse(b.start_time) - Date.parse(a.start_time)
@@ -30,20 +26,21 @@ class FreelanceJobsContainer extends React.Component {
         return <OpenFreelanceCard key={job.id} job={job} />});
     };
     
-    openJobsForMap = () => {
-        return this.props.user.jobs_as_freelancer.filter((job) => job.completed === false);
-
+    const openJobsForMap = () => {
+        return user.jobs_as_freelancer.filter((job) => job.completed === false);
     }
 
-    componentWillUnmount = () => {
-        this.props.setStartDateForFilter('')
-        this.props.setEndDateForFilter('')
-    }
+    useEffect(() => {
+        return () => {
+            setStartDateForFilter('');
+            setEndDateForFilter('')
+        }
+    }, [setStartDateForFilter, setEndDateForFilter])
   
-	render() {
+	
 		return (
 			<Container>
-                <GoogleMap jobs={this.openJobsForMap()}/>
+                <GoogleMap jobs={openJobsForMap()}/>
 				<Table bordered className="mt-2">
 					<thead>
 						<tr>
@@ -52,7 +49,7 @@ class FreelanceJobsContainer extends React.Component {
 					</thead>
 				</Table>
 				<Table bordered hover>
-					<thead>
+					<thead> 
 						<tr>
 							<th>Description</th>
 							<th>Client Email</th>
@@ -68,65 +65,16 @@ class FreelanceJobsContainer extends React.Component {
 						</tr>
 					</thead>
 					<tbody>
-                        {this.usersOpenFreelanceJobs()}
-						
+                        {usersOpenFreelanceJobs()}
 					</tbody>
 				</Table>
 
-				{/* <Table bordered>
-					<thead>
-						<tr>
-							<th style={{fontSize: 14}}>Completed Freelance Jobs</th>
-						</tr>
-					</thead>
-				</Table> */}
-
-<DateFilterAndExcelRow tableHeader='Completed Freelance Jobs' tableTitle='closed-freelance-jobs' filename='freelanceJobs' />
-
-{/* <Table bordered className="mt-2">
-					<thead>
-						<tr>
-							<th style={{fontSize: 14}}>Completed Freelance Jobs</th>
-						</tr>
-						<tr >
-							<th >
-                            <Row className='mb-2'>
-                                <Col className="d-flex align-items-center">
-								
-                                <ReactToExcel 
-                                    table='closed-freelance-jobs'
-                                    filename='freelanceJobs'
-                                    sheet='sheet 1'
-                                    className='btn ml-2'
-                                    id='exlButton'
-                                    buttonText='Export To XLS'
-                                />
-                                </Col>
-                                </Row>
-                                <Row>
-                                    <Col className = 'd-flex align-items-center'>
-                                Start Date: <DatePicker className='dateSort'
-                                onChange={this.startDateChangeHandler} selected={this.state.startDate}
-                                /> 
-                                End Date: <DatePicker className='dateSort' 
-                                onChange={this.endDateChangeHandler} selected={this.state.endDate}
-                                /><Button 
-                                onClick={this.resetDate}
-                                style={{ fontSize: 12 , marginLeft: 6}}>Reset</Button>
-                                
-                                </Col>
-                                
-                        </Row>
-                        </th>
-						</tr>
-					</thead>
-				</Table> */}
-				
+                <DateFilterAndExcelRow tableHeader='Completed Freelance Jobs' tableTitle='closed-freelance-jobs' filename='freelanceJobs' />
 
 				<Table id='closed-freelance-jobs' bordered hover>
 					<thead>
 						<tr>
-						<th>Description</th>
+						    <th>Description</th>
 							<th>Client Email</th>
 							<th>Start Time</th>
 							<th>Hourly/Day</th>
@@ -137,12 +85,11 @@ class FreelanceJobsContainer extends React.Component {
 						</tr>
 					</thead>
 					<tbody>
-                    {this.usersCompletedFreelanceJobs()}
+                    {usersCompletedFreelanceJobs()}
 					</tbody>
 				</Table>
 			</Container>
 		);
-	}
 }
 
 const mapStateToProps = (state) => {
