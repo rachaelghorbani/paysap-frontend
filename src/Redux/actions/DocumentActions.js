e72519635d5ddb0f7a23207ac906eadc55882dcc
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-11-06 17:55:24 -0500

    cleaned up App.css and pulled out options function and token in document actions

--- src/Redux/actions/DocumentActions.js
+++ src/Redux/actions/DocumentActions.js
@@ -1,90 +1,79 @@
+const token = localStorage.getItem('token');
 
-export const addDocument = formData => {
-    return function(dispatch, getState){
+const options = (method, data) => {
+    return {
+        method: method,
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        body: data
+    }
+}
 
-        // const token = localStorage.getItem('token');
-
-        const options = {
-            method: 'POST',
-            // headers: {
-			// 	Authorization: `Bearer ${token}`
-			// },
-			body: formData
-        };
-
-
-        fetch('http://localhost:3000/documents', options)
+export const addDocument = (formData) => {
+	return function(dispatch, getState) {
+        fetch('http://localhost:3000/documents', options('POST', formData))
         .then((resp) => resp.json())
         .then((document) => {
-            console.log(document)
-            const formattedJob = {
-                id: document.id,
-                description: document.description,
-                pdf_url: document.pdf_url,
-                preview_url: document.preview_url
-            }
-            const user = {...getState().user, documents:[...getState().user.documents, formattedJob]}
-            return dispatch({type: "ADD_DOCUMENT", payload: user})
+			console.log(document);
+			const formattedJob = {
+				id: document.id,
+				description: document.description,
+				pdf_url: document.pdf_url,
+				preview_url: document.preview_url
+			};
+			const user = { ...getState().user, documents: [ ...getState().user.documents, formattedJob ] };
+			return dispatch({ type: 'ADD_DOCUMENT', payload: user });
 		});
-    }
-}
-
-export const updatePdfUrl = pdf => {
-    return {
-        type: 'PDF_URL',
-        payload: pdf
-    }
-}
-
-export const hideThumbnails = () => {
-    return {
-        type: 'HIDE_THUMBNAILS',
-        payload: true
-    }
-}
-export const showThumbnails = () => {
-    return {
-        type: 'SHOW_THUMBNAILS',
-        payload: false
-    }
-}
+	};
+};
 
 export const deleteDocument = (docObj, id) => {
-    return function(dispatch, getState){
-        const token = localStorage.getItem('token');
-        console.log(docObj)
-        const options = {
-            method: 'DELETE',
-            headers: {
-            	Authorization: `Bearer ${token}`
-            },
-            body: docObj
-        }
-        fetch(`http://localhost:3000/documents/${id}`, options)
-        .then(resp => resp.json())
-        .then((retdoc) => {
-            const documents = getState().user.documents
-            const filtered = documents.filter(doc => doc.id !== id)
-            const newArr = {
-                ...getState().user,
-                documents: filtered
-            };
-            console.log(retdoc)
-            return dispatch({type: 'DELETE_DOCUMENT', payload: newArr})
-        })
-    }
-}
+	return function(dispatch, getState) {
+        fetch(`http://localhost:3000/documents/${id}`, options('DELETE', docObj))
+        .then((resp) => resp.json())
+        .then(() => {
+			const documents = getState().user.documents;
+			const filtered = documents.filter((doc) => doc.id !== id);
+			const newArr = {
+				...getState().user,
+				documents: filtered
+			};
+			return dispatch({ type: 'DELETE_DOCUMENT', payload: newArr });
+		});
+	};
+};
 
 export const showUploadForm = () => {
-    return {
-        type: 'SHOW_UPLOAD_FORM',
-        payload: true
-    }
-}
+	return {
+		type: 'SHOW_UPLOAD_FORM',
+		payload: true
+	};
+};
 
 export const hideUploadForm = () => {
-    return {
-        type: 'HIDE_UPLOAD_FORM',
-        payload: false 
-    }
-}
+	return {
+		type: 'HIDE_UPLOAD_FORM',
+		payload: false
+	};
+};
+
+export const updatePdfUrl = (pdf) => {
+	return {
+		type: 'PDF_URL',
+		payload: pdf
+	};
+};
+
+export const hideThumbnails = () => {
+	return {
+		type: 'HIDE_THUMBNAILS',
+		payload: true
+	};
+};
+export const showThumbnails = () => {
+	return {
+		type: 'SHOW_THUMBNAILS',
+		payload: false
+	};
+};
